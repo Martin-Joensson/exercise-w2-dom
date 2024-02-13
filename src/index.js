c1803c4be57bda0b622c299064f4c48cf3107cc3
Martin Jönsson <martin.t.joensson@gmail.com>
2024-02-13 13:32:25 +0100

    Change color complete
    Co-authored-by: Wen Z <wwenzz@users.noreply.github.com>
    Co-authored-by: Caracal23 <Caracal23@users.noreply.github.com>
    Co-authored-by: Mai Kanetaka <maikanetaka@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log("Start Here 😉");
+//Dom Selectors
+const box = document.getElementById("box");
+const dark = document.getElementById("dark");
+const light = document.getElementById("light");
+const crazy = document.getElementById("crazy");
+
+// Functions
+const changeToDark = () => {
+  box.style.backgroundColor = "#663399";
+  box.style.transitionDuration = "3s"
+}
+
+const changeToLight= () => {
+  box.style.backgroundColor = "#ccc";
+}
+
+const changeToCrazy= () => {
+    box.style.backgroundColor = "lightgreen";
+}
+
+
+
+// Event listners
+dark.addEventListener("click", changeToDark)
+light.addEventListener("click", changeToLight)
+crazy.addEventListener("click", changeToCrazy)
+
